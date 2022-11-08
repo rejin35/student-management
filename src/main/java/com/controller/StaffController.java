80d9b4a7e3ecd7f894182f871e1778564f677169
Hp <Hp@DESKTOP-U6KNCOL>
2022-11-08 11:25:03 +0530

    Basic Structure

--- src/main/java/com/controller/StaffController.java
+++ src/main/java/com/controller/StaffController.java
@@ -1,9 +1,10 @@
 package com.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("com.*")
 public class StaffController {
 
 }
--- src/main/java/com/controller/StudentController.java
+++ src/main/java/com/controller/StudentController.java
@@ -1,8 +1,35 @@
 package com.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.model.Student;
+
 @RestController
+@RequestMapping("com.*")
 public class StudentController {
+	
+	@PostMapping("/addstudent")
+	Student addstudent(@RequestBody Student student) {
+		return student;
+	}
+	@GetMapping("/addstudent")
+	Student addstudent1(@RequestBody Student student) {
+		return student;
+	}
+	@PutMapping("/addstudent")
+	Student addstudent2(@RequestBody Student student) {
+		return student;
+	}
+	@DeleteMapping("/addstudent")
+	Student addstudent3(@RequestBody Student student) {
+		return student;
+	}
+	
 
 }
