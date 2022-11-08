493353aaabdc636badbb5c1fed225d1bfca0c119
Hp <Hp@DESKTOP-U6KNCOL>
2022-11-08 10:33:18 +0530

    Structure

---
+++ src/main/java/com/repository/StaffRepository.java
@@ -0,0 +1,9 @@
+package com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.model.Staff;
+
+public interface StaffRepository extends JpaRepository<Staff, String> {
+
+}
